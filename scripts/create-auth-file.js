7d3e46246e61b9f34b97958ff9874223fcce4632
Plus664 <soccer710a@gmail.com>
2025-09-28 11:56:08 +0900

    Add files via upload

---
+++ scripts/create-auth-file.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+const password = process.env.ACCESS_PASSWORD;
+
+const fileContent = `
+// このファイルはビルド時に自動生成されます
+const ACCESS_PASSWORD = "${password}";
+`;
+
+fs.writeFileSync("./javascript/auth-config.js", fileContent);
