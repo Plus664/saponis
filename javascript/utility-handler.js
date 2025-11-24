4570a31073aae7c7fbaea891d37e07a166ba3945
Plus664 <soccer710a@gmail.com>
2025-11-24 14:05:52 +0900

    Update utility-handler.js

--- javascript/utility-handler.js
+++ javascript/utility-handler.js
@@ -4,7 +4,7 @@
 function checkPassword() {
   const input = document.getElementById("password-input").value;
 
-  if(input === password) {alert("直す")
+  if(input === ACCESS_PASSWORD) {
     document.getElementById("overlay-lock").style.display = "none";
     sessionStorage.setItem("isMemberAuthenticated", "true");
   } else {
@@ -94,3 +94,4 @@ function createLoginOverlay() {
   document.body.insertAdjacentHTML("afterbegin", overlayHTML);
 
 }
+
