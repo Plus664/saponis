f9f8af5f65b9a19f1ff133f0efe14d2cb1d669ac
Plus664 <soccer710a@gmail.com>
2026-01-24 19:33:31 +0900

    Delete javascript/supabase.js

--- javascript/supabase.js
+++
@@ -1,9 +0,0 @@
-// ===============================
-// Supabase
-// ===============================
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const supabase = createClient(
-  'https://rmbbsrfstmnfxbbttaro.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtYmJzcmZzdG1uZnhiYnR0YXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNDc0OTgsImV4cCI6MjA4NDcyMzQ5OH0.ELoVUxFgbWxaUJDg1DziRp0Y4cSo5MX2zEUDO2bIEzk'
-);
