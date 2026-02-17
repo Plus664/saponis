fe7024cf0a212f8898545927c5e66b3993359794
Plus664 <soccer710a@gmail.com>
2026-02-17 22:04:37 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 // ===============================
+// service-worker
+// ===============================
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then((registration) => {
+                console.log("SW registered", registration);
+            })
+            .catch((error) => {
+                console.error("SW registration failed:", error);
+            });
+    });
+}
+
+// ===============================
 // Supabase
 // ===============================
 const sb = supabase.createClient(
